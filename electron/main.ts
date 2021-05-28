fa2007f1d148320abc1e2bc2cc538b42c2bbb05a
Curtis Sapp <curtmsapp@gmail.com>
2021-05-27 19:18:40 -0700

    Dev Tools Now Open

--- electron/main.ts
+++ electron/main.ts
@@ -16,6 +16,7 @@ function createWindow() {
 
   if (process.env.NODE_ENV === 'development') {
     mainWindow.loadURL(`http://localhost:4000`);
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadURL(
       url.format({
